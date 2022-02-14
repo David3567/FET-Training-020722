aad4e750db013b184ded360da04ebfeb232a8b76
kuku20 <lluu3790@gmail.com>
2022-02-14 11:47:38 -0600

    just htm

--- as4-javascript/index.js
+++ as4-javascript/index.js
@@ -12,7 +12,7 @@ function reverse(num) {
     return result;
   }
   console.log("======================");
-  console.log('1. Reverse number of 234 is:', reverse(32243));
+  console.log('1. Reverse number of 32243 is:', reverse(32243));
   /*
   2. Write a JavaScript function that checks whether a passed string is palindrome or not? 
   A palindrome is word, phrase, or sequence that reads the same backward as forward, e.g.,
