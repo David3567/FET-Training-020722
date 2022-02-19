41dc78c613e4df41b73d33791bd5d7d2ae2a24df
lukeyzhong <lukeyzhong@gmail.com>
2022-02-19 17:12:05 -0600

    mvc

--- HW/HW5/index.js
+++ HW/HW5/index.js
@@ -1,41 +1,71 @@
-const state = {
-    a: "apple",
-    b: "banana",
-    c: "city",
-    d: "dom",
-    e: "end",
-    f: "forEach"
-};
+const View = (() => {
+    const domElements = {
+        shortNames: document.querySelector('#shortNames'),
+        fullNames: document.querySelector('#fullNames')
+    }
 
-const shortNames = document.getElementById('shortNames');
-const fullNames = document.getElementById('fullNames');
-
-// render method 
-const render = (element, tmp) => {
-    element.innerHTML = tmp;
+    const render = (element, tmp) => {
+        element.innerHTML = tmp;
+    }
+    return {
+        domElements,
+        render
+    }
 }
-// modify html
-let tmp1 = '';
-for (let shortName of Object.keys(state)) {
-    tmp1 += `<option value="${shortName}">${shortName}</option>`;
+)();
+
+const Model = (() => {
+    const state = {
+        a: "apple",
+        b: "banana",
+        c: "city",
+        d: "dom",
+        e: "end",
+        f: "forEach"
+    };
+    return {state};
 }
-render(shortNames, tmp1);
+)();
 
-let tmp2 = '';
-for (let shortName of Object.keys(state)) {
-    tmp2 += `<option value="${shortName}">${state[shortName]}</option>`;
+const Controller = ((view, model) => {
+    // console.log(Object.keys(model.state));
+    const createKeyList = () => {
+        let tmp = '';
+        for (let shortName of Object.keys(model.state)) {
+            tmp += `<option value="${shortName}">${shortName}</option>`;
+        }
+        //现在的这个是id 也可以是那个变量
+        console.log(model.state);
+        view.render(shortNames, tmp);
+        
+    }
+    
+
+    const createValueList = () => {
+        let tmp = '';
+        for (let shortName of Object.keys(model.state)) {
+            tmp += `<option value="${shortName}">${model.state[shortName]}</option>`;
+        };
+        view.render(fullNames, tmp);
+        
+    }
+
+    const setupEvent = () => {
+        view.domElements.shortNames.addEventListener('change', () => {
+            view.domElements.fullNames.value = view.domElements.shortNames.value;
+        });
+        view.domElements.fullNames.addEventListener('change', () => {
+            view.domElements.shortNames.value = view.domElements.fullNames.value;
+        });
+    }
+    
+    const init = () => {
+        createKeyList();
+        createValueList();
+        setupEvent();
+    }
+    return {init};
 }
-render(fullNames, tmp2);
+)(View, Model);
 
-//addEventListener
-shortNames.addEventListener('change', change1)
-
-function change1 () {
-    fullNames.value = shortNames.value;
-}
-
-fullNames.addEventListener('change', change2)
-
-function change2 () {
-    shortNames.value = fullNames.value;
-}
+Controller.init();
