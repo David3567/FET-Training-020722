5f52d21988499c895153619fc93a8f6662787c63
jhxiaoo <junhui.xiaoo@gmail.com>
2022-02-21 17:32:45 -0500

    Evaluation

---
+++ Evaluation/index.js
@@ -0,0 +1,142 @@
+const Api = (() => {
+    const baseUrl = "http://localhost:3000";
+    const movie = "movies";
+
+    const getMovies = () =>
+        fetch([baseUrl, movie].join("/")).then((response) => response.json());
+
+    return {
+        getMovies,
+    };
+})();
+
+
+const View = (() => {
+    const domStr = {
+        movielist: ".movie-holder",
+        movie: ".movie.container",
+        rightbn: ".move-right",
+        leftbn: ".move-left"
+    };
+    const render = (ele, tmp) => {
+        ele.innerHTML = tmp;
+    };
+    const createTmp = (arr) => {
+        let tmp = "";
+        arr.forEach((movie) => {
+            tmp += `
+                <div class="movie-container" id ="${movie.id}">
+                    <img src="${movie.imgUrl}">
+                    <p>${movie.title}</p>
+                    <p>${movie.updateInfo}</p>
+                </div>
+            `;
+        });
+        return tmp;
+    };
+    const show = (focus, viewnum) => {
+        for (let i = focus; i < focus + viewnum; i++) {
+            let ele = document.getElementById(i);
+            ele.style.display = "block";
+            ele.style.flex = "25%";
+        }
+    };
+
+    return {
+        domStr,
+        render,
+        createTmp,
+        show
+    };
+})();
+
+const Model = ((Api, View) => {
+
+    class State {
+        #movielist = [];
+
+        get movielist() {
+            return this.#movielist;
+        }
+        set movielist(newmovie) {
+            this.#movielist = newmovie;
+            const tmp = View.createTmp(this.#movielist);
+            const movielist = document.querySelector(View.domStr.movielist);
+            View.render(movielist, tmp);
+        }
+    }
+
+    const getMovies = Api.getMovies;
+
+    return {
+        getMovies,
+        State,
+    };
+})(Api, View);
+
+
+const appController = ((Model, View) => {
+    const state = new Model.State();
+    const movie = document.querySelector(View.domStr.movie);
+    const leftbn = document.querySelector(View.domStr.leftbn);
+    const rightbn = document.querySelector(View.domStr.rightbn);
+
+    let viewnum = 4;
+    let focus = 1;
+
+    const leftMove = () => {
+        leftbn.addEventListener("click", (event) => {
+            let ele = document.getElementById(focus + viewnum - 1);
+            ele.style.display = "none";
+            focus--;
+            if (focus <= 0) {
+                focus = 1;
+            }
+            View.show(focus, viewnum);
+            rightbn.style.display = "block"
+            if (focus === 1) {
+                leftbn.style.display = "none";
+
+            } else {
+                leftbn.style.display = "block";
+            }
+        })
+    };
+
+    const rightMove = () => {
+        rightbn.addEventListener("click", (event) => {
+            let ele = document.getElementById(focus);
+            ele.style.display = "none";
+            focus++;
+            if (focus > state.movielist.length - viewnum + 1) {
+                focus = state.movielist.length - viewnum + 1;
+            }
+            View.show(focus, viewnum);
+            leftbn.style.display = "block";
+            if (focus === state.movielist.length - viewnum + 1) {
+                rightbn.style.display = "none";
+            } else {
+                rightbn.style.display = "block";
+            }
+        })
+    };
+
+    const init = () => {
+        Model.getMovies().then((movies) => {
+            state.movielist = movies;
+            View.show(1, 4);
+        });
+    };
+
+    const bootstrap = () => {
+        init();
+        leftMove();
+        rightMove();
+    };
+
+    return {
+        bootstrap
+    };
+})(Model, View);
+
+appController.bootstrap();
