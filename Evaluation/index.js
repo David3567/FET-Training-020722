6850cdc0f3b6fc1e7d9d561f00e3424dfcb37d16
jhxiaoo <junhui.xiaoo@gmail.com>
2022-02-24 17:01:36 -0500

    UPDATE

--- Evaluation/index.js
+++ Evaluation/index.js
@@ -5,8 +5,26 @@ const Api = (() => {
     const getMovies = () =>
         fetch([baseUrl, movie].join("/")).then((response) => response.json());
 
+    const deleteMovies = (id) => {
+        // console.log("id", id);
+        fetch([baseUrl, movie, id].join("/"), {
+            method: "DELETE",
+        });
+    }
+    const addMovies = (newMovie) => {
+        fetch([baseUrl, movie].join("/"), {
+            method: "POST",
+            body: JSON.stringify(newMovie),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8",
+            }
+        }).then((response) => response.json());
+    }
+
     return {
         getMovies,
+        deleteMovies,
+        addMovies
     };
 })();
 
@@ -14,9 +32,9 @@ const Api = (() => {
 const View = (() => {
     const domStr = {
         movielist: ".movie-holder",
-        movie: ".movie.container",
         rightbn: ".move-right",
-        leftbn: ".move-left"
+        leftbn: ".move-left",
+        addform: ".add-movie-container",
     };
     const render = (ele, tmp) => {
         ele.innerHTML = tmp;
@@ -29,16 +47,24 @@ const View = (() => {
                     <img src="${movie.imgUrl}">
                     <p>${movie.title}</p>
                     <p>${movie.updateInfo}</p>
+                    <button class="delete-btn" name="${movie.id}">X</button>
                 </div>
             `;
         });
         return tmp;
     };
-    const show = (focus, viewnum) => {
+    const show = (state, focus, viewnum) => {
+        // console.log(state);
+        // console.log(focus);
+        // console.log(state.movielist[0]);
+
         for (let i = focus; i < focus + viewnum; i++) {
-            let ele = document.getElementById(i);
+
+            let ele = document.getElementById(state.movielist[i].id);
+            // console.log(ele);
             ele.style.display = "block";
         }
+
     };
 
     return {
@@ -50,6 +76,14 @@ const View = (() => {
 })();
 
 const Model = ((Api, View) => {
+    class Movie {
+        constructor(imgUrl, title, updateInfo) {
+            this.id = NaN;
+            this.imgUrl = imgUrl;
+            this.title = title;
+            this.updateInfo = updateInfo;
+        }
+    };
 
     class State {
         #movielist = [];
@@ -66,10 +100,15 @@ const Model = ((Api, View) => {
     }
 
     const getMovies = Api.getMovies;
+    const deleteMovies = Api.deleteMovies;
+    const addMovies = Api.addMovies;
 
     return {
         getMovies,
         State,
+        Movie,
+        deleteMovies,
+        addMovies,
     };
 })(Api, View);
 
@@ -78,64 +117,127 @@ const appController = ((Model, View) => {
     const state = new Model.State();
     const leftbn = document.querySelector(View.domStr.leftbn);
     const rightbn = document.querySelector(View.domStr.rightbn);
-
+    const movielist = document.querySelector(View.domStr.movielist);
+    const form = document.querySelector(View.domStr.addform);
+    // console.log(state.movielist);
     let viewnum = 4;
-    let focus = 1;
+    let focus = 0;
 
     const leftMove = () => {
         leftbn.addEventListener("click", (event) => {
-            let ele = document.getElementById(focus + viewnum - 1);
+            let ele = document.getElementById(state.movielist[focus + viewnum - 1].id);
             ele.style.display = "none";
             focus--;
-            if (focus <= 0) {
-                focus = 1;
-            }
-            View.show(focus, viewnum);
-            rightbn.style.display = "block"
-            if (focus === 1) {
-                leftbn.style.display = "none";
-
-            } else {
-                leftbn.style.display = "block";
-            }
+            focus = checkfocus(state, focus, viewnum).focus;
+            viewnum = checkfocus(state, focus, viewnum).viewnum;
+            View.show(state, focus, viewnum);
         })
     };
 
     const rightMove = () => {
         rightbn.addEventListener("click", (event) => {
-            let ele = document.getElementById(focus);
+            // console.log(focus);
+            let ele = document.getElementById(state.movielist[focus].id);
             ele.style.display = "none";
             focus++;
-            if (focus > state.movielist.length - viewnum + 1) {
-                focus = state.movielist.length - viewnum + 1;
-            }
-            View.show(focus, viewnum);
-            leftbn.style.display = "block";
-            if (focus === state.movielist.length - viewnum + 1) {
-                rightbn.style.display = "none";
-            } else {
-                rightbn.style.display = "block";
-            }
+            focus = checkfocus(state, focus, viewnum).focus;
+            viewnum = checkfocus(state, focus, viewnum).viewnum;
+            View.show(state, focus, viewnum);
+
         })
+    };
+
+    const deleteMovie = () => {
+        movielist.addEventListener("click", (event) => {
+            if (event.target.nodeName === "BUTTON") {
+                console.log(`success deleted id ${event.target.name}.`);
+                state.movielist = state.movielist.filter(
+                    (movie) => +movie.id !== +event.target.name
+                );
+                Model.deleteMovies(event.target.name);
+                focus = checkfocus(state, focus, viewnum).focus;
+                viewnum = checkfocus(state, focus, viewnum).viewnum;
+                View.show(state, focus, viewnum);
+            }
+        });
+    };
+
+    const addMovie = () => {
+        form.addEventListener("submit", (event) => {
+            let imgUrl = document.forms["add-movie"]["add-movie-img-url"].value;
+            let title = document.forms["add-movie"]["add-movie-title"].value;
+            let updateInfo = document.forms["add-movie"]["add-movie-updateInfo"].value;
+
+            if (imgUrl != "" && title != "" && updateInfo != "") {
+                const movie = new Model.Movie(imgUrl, title, updateInfo);
+                Model.addMovies(movie).then((movie) => {
+                    state.movielist = [...state.movielist, movie];
+                })
+                focus = checkfocus(state, focus, viewnum).focus;
+                viewnum = checkfocus(state, focus, viewnum).viewnum;
+                View.show(state, focus, viewnum);
+            }else{
+                event.preventDefault();
+                alert("Please input all infos!");
+            }
+
+        });
+    };
+
+    const checkfocus = (state, focus, viewnum) => {
+        if (state.movielist.length <= viewnum) {
+            viewnum = state.movielist.length;
+        }
+
+        if (focus < 0) {
+            focus = 0;
+        }
+
+        if (focus + viewnum > state.movielist.length) {
+            focus = state.movielist.length - viewnum;
+        }
+
+        if (focus === 0) {
+            leftbn.style.display = "none";
+        } else {
+            leftbn.style.display = "block";
+        }
+
+        if (focus + viewnum === state.movielist.length) {
+            rightbn.style.display = "none";
+        } else {
+            rightbn.style.display = "block";
+        }
+
+        if (viewnum < 4) {
+            movielist.style.justifyContent = "flex-start";
+        }
+
+        return {
+            state,
+            focus,
+            viewnum
+        };
     };
 
     const init = () => {
         Model.getMovies().then((movies) => {
             state.movielist = movies;
-            if(state.movielist.length <= viewnum){
-                viewnum = state.movielist.length;
-                leftbn.style.display = "none";
-                rightbn.style.display = "none";
-            }
-            View.show(focus, viewnum);
+            focus = checkfocus(state, focus, viewnum).focus;
+            viewnum = checkfocus(state, focus, viewnum).viewnum;
+            View.show(state, focus, viewnum);
+            console.log(state.movielist);
+
         });
-        
+
     };
 
     const bootstrap = () => {
         init();
         leftMove();
         rightMove();
+        deleteMovie();
+        addMovie();
     };
 
     return {
